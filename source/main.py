4ab5ead63be754957fcfc1d03efabf0b455ec39b
1lyasm <il.mystafa@gmail.com>
2023-03-22 18:43:47 +0400

    Set up source and tests

---
+++ source/main.py
@@ -0,0 +1,9 @@
+def add_ints(a, b):
+	return a + b
+
+def main():
+	print("hey\n")
+	return 0;
+
+if __name__ == "__main__":
+	main()
---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append("../source")
+
+from source.main import *	
+
+def test_main():
+	assert(main() == 0)
+
+def test_add_ints():
+	assert(add_ints(1, 3) == 4)
