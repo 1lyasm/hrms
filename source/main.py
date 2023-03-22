792bf2be0c55701d6f0b7be6d15d040f70f0852f
1lyasm <il.mystafa@gmail.com>
2023-03-22 19:41:15 +0400

    Configure python source and tests

---
+++ source/main.py
@@ -0,0 +1,8 @@
+def add_ints(int_1, int_2):
+	return int_1 + int_2
+
+def main():
+	print("hey")
+	return 0
+
+if __name__ == "__main__": main()
---
+++ tests/test_main.py
@@ -0,0 +1,7 @@
+from source.main import *
+
+def test_main():
+	assert(main() == 0)
+
+def test_add_ints():
+	assert(add_ints(1, 3) == 4)
