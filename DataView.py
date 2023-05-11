b32d9209e9c24b298cf02439e411d04838513992
1lyasm <il.mystafa@gmail.com>
2023-05-11 17:21:28 +0400

    Clean up DataView

--- DataView.py
+++ DataView.py
@@ -1,40 +1,28 @@
 import pandas as pd
 import numpy as np
 
-class DFAugmenter:
+
+def augmentDF(DF):
     ColumnsTypes = {"Name": "object", "Gender": "object", "Role": "object",
                     "Salary": "float64", "Age": "Int64",
                     "Education": "object", "Distance": "Int64",
                     "Remote": "boolean", "Marital status": "object",
                     "Smoking": "boolean", "Driver license": "boolean",
                     "Language skills": "object", "Employed at": "datetime64[ns]"}
-
     TypeNullVal = {
-        "Int64": pd.NA,
-        "float64": np.nan,
-        "boolean": np.nan,
-        "datetime64[ns]": pd.NaT,
-        "object": np.nan
+    "Int64": pd.NA,
+    "float64": np.nan,
+    "boolean": np.nan,
+    "datetime64[ns]": pd.NaT,
+    "object": np.nan
     }
-
-    @staticmethod
-    def augment(DF):
-        PrevCols = DF.columns
-        UpdatedDF = DF
-        for Column, Type in DFAugmenter.ColumnsTypes.items():
-            if not Column in PrevCols:
-                UpdatedDF[Column] = DFAugmenter.TypeNullVal[Type]
-            UpdatedDF[Column] = UpdatedDF[Column].astype(Type)
-        return UpdatedDF
-
-class ExcelIO:
-    @staticmethod
-    def readDF(FileName):
-        return pd.read_excel(FileName)
-
-    @staticmethod
-    def writeDF(FileName, DF):
-        DF.to_excel(FileName, index=False)
+    PrevCols = DF.columns
+    UpdatedDF = DF
+    for Column, Type in ColumnsTypes.items():
+        if not Column in PrevCols:
+            UpdatedDF[Column] = TypeNullVal[Type]
+        UpdatedDF[Column] = UpdatedDF[Column].astype(Type)
+    return UpdatedDF
 
 
 def isAnyNullType(Obj):
@@ -43,3 +31,10 @@ def isAnyNullType(Obj):
         if Obj is Type:
             return True
     return False
+
+# def readDF(FileName):
+#     return pd.read_excel(FileName)
+
+# def writeDF(FileName, DF):
+#     DF.to_excel(FileName, index=False)
+
